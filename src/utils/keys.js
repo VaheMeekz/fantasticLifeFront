890acf964cbf4af4472e2779dd38e987bd960143
StepanKhachikyan1999 <[stepan-khachikyan@mail.ru]>
2022-06-03 18:08:12 +0400

    banaliz

--- src/utils/keys.js
+++ src/utils/keys.js
@@ -4,9 +4,14 @@ export const errorTypes = {
     text: 'Text'
 }
 
-export const API_URI = "https://fantasticlife.herokuapp.com/api/v1"
+export const API_URI = "https://fantasticlife.herokuapp.com/api/v1"    
 export const WS_URI = "ws://localhost:8900"
 export const token = localStorage.getItem("myTokenSport")
 export const userId = localStorage.getItem("userId")
 
 export const myUrl = "https://fantastic-life-front.vercel.app"
+
+
+
+
+
