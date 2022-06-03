a5351df6b4695d83f04828c3fd196cb9f3e84615
StepanKhachikyan1999 <[stepan-khachikyan@mail.ru]>
2022-06-03 18:01:59 +0400

    vercel

--- src/utils/keys.js
+++ src/utils/keys.js
@@ -9,4 +9,4 @@ export const WS_URI = "ws://localhost:8900"
 export const token = localStorage.getItem("myTokenSport")
 export const userId = localStorage.getItem("userId")
 
-export const myUrl = "https://fantastic-life-front.vercel.app/"
+export const myUrl = "https://fantastic-life-front.vercel.app"
